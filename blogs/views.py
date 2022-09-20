8b1aa2191d6676c292e59b492c5b89a4077a539a
Ramniwas Gupta <ramniwas.nitrr15@gmail.com>
2022-09-21 01:25:30 +0530

    minor imrovement

--- blogs/views.py
+++ blogs/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from projects.models import Projects
 from .models import Post
 
-def getdate(post):
-    return post['date']
-
 def staring_page(request):
-    latest_posts = Post.objects.all().order_by('-date')[:3]
+    latest_posts = Post.objects.all().order_by('date')[:3]
+    latest_projects = Projects.objects.all().order_by('date')[:3]
     return render(request, 'blogs/index.html', {
-        "posts": latest_posts
+        "posts": latest_posts,
+        "projects": latest_projects
     })
 
 def all_posts(request):
